fa4637222682b9d199d151bf0c4e4cbcffd8726c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-02 08:31:10 +0200

    Configuration class

---
+++ src/cz/tisnik/dxf2sap/Configuration.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cz.tisnik.dxf2sap;
+
+/**
+ *
+ * @author Pavel Tisnovsky
+ */
+public class Configuration
+{
+    public static final boolean SHOW_CONFIRMATION_DIALOG_ON_EXIT = false;
+    public static final int MAIN_WINDOW_WIDTH = 800;
+    public static final int MAIN_WINDOW_HEIGHT = 600;
+
+    public static final boolean SYSTEM_LOOK_AND_FEEL = false;
+}

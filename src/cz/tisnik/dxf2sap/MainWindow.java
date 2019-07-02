601b8216ab2e185a8f763b8bc2c3da6b578f09fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-02 08:31:22 +0200

    MainWindow class

---
+++ src/cz/tisnik/dxf2sap/MainWindow.java
@@ -0,0 +1,16 @@
+package cz.tisnik.dxf2sap;
+
+import cz.tisnik.dxf2sap.datamodel.DataModel;
+
+public class MainWindow {
+
+	public MainWindow(DataModel dataModel) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
